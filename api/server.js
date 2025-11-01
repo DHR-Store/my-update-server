3530673e296c9b5d3ae72032a00c35a3f20a51b9
Dr. Doom <kalipusur1234@gmail.com>
2025-11-02 02:09:26 +0530

    Update release version and notes in server.js

--- api/server.js
+++ api/server.js
@@ -14,15 +14,15 @@ app.get('/', (req, res) => {
 // Hardcoded release data for demonstration purposes.
 const releases = [
   {
-    version: "3.3.5",
-    releaseNotes: "sorry for ping\n navbar fixed\n multiple fast foreword option\n + watchlist share option and pastelink feature u can share your fav content with ur fr\n -😁😁💖Thanks🤞🤞",
+    version: "3.3.6",
+    releaseNotes: "sorry for ping\n background downloader fixed\n multiple fast foreword option 2x 3x 4x \n + u can play ur loacal video using app player in download section\n -😁😁💖Thanks🤞🤞",
     // The downloadUrl is now set to your webpage link to open the page on update click.
     downloadUrl: "https://dhr-store.vercel.app/app2.html",
     fileName: "your-app-v3.2.5.apk",
     publishedAt: "2025-08-06T12:00:00Z"
   },
   {
-    version: "3.3.4",
+    version: "3.3.5",
     releaseNotes: "Bug fixes and performance improvements.\n- Fixed login issue\n- Improved UI responsiveness",
     downloadUrl: "https://your-vercel-app-domain.vercel.app/downloads/your-app-v1.0.1.apk",
     fileName: "your-app-v1.0.1.apk",
