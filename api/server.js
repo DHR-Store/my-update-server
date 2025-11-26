7114f45f8cb851ed6704c2a06b04174b70b5e803
Dr. Doom <kalipusur1234@gmail.com>
2025-11-26 17:38:24 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -14,15 +14,15 @@ app.get('/', (req, res) => {
 // Hardcoded release data for demonstration purposes.
 const releases = [
   {
-    version: "3.4.01",
-    releaseNotes: " \n fix room ID  and setting baar url \n -😁😁💖Thanks🤞🤞",
+    version: "3.4.1",
+    releaseNotes: " \n +all history storage issues fixed and continue watch history fixed \n +added private and public mode for sync video \n -😁😁💖Thanks🤞🤞",
     // The downloadUrl is now set to your webpage link to open the page on update click.
     downloadUrl: "https://dhr-store.vercel.app/app2.html",
     fileName: "your-app-v3.2.5.apk",
     publishedAt: "2025-08-06T12:00:00Z"
   },
   {
-    version: "3.4.0",
+    version: "3.4.01",
     releaseNotes: "Bug fixes and performance improvements.\n- Fixed login issue\n- Improved UI responsiveness",
     downloadUrl: "https://your-vercel-app-domain.vercel.app/downloads/your-app-v1.0.1.apk",
     fileName: "your-app-v1.0.1.apk",
